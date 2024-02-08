75d9d50680ca64bc2086b129c4b39dc90b4f53d9
neverland <chenjiahan.jait@bytedance.com>
2024-02-08 14:19:21 +0800

    feat: bump Rsbuild 0.4.0

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,4 +4,19 @@ import { pluginSvgr } from '@rsbuild/plugin-svgr';
 
 export default defineConfig({
   plugins: [pluginReact(), pluginSvgr({ svgDefaultExport: 'component' })],
+  source: {
+    transformImport: [
+      {
+        libraryName: '@arco-design/web-react',
+        libraryDirectory: 'es',
+        camelToDashComponentName: false,
+        style: true,
+      },
+      {
+        libraryName: '@arco-design/web-react/icon',
+        libraryDirectory: 'react-icon',
+        camelToDashComponentName: false,
+      },
+    ],
+  },
 });
