afcdec396a2c5f42dd6b3799214bdf9fc508bccb
neverland <chenjiahan.jait@bytedance.com>
2024-05-29 11:17:41 +0800

    chore: bump Rsbuild v0.7

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from '@rsbuild/core';
+import { pluginLess } from '@rsbuild/plugin-less';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSvgr } from '@rsbuild/plugin-svgr';
 
 export default defineConfig({
   plugins: [
+    pluginLess(),
     pluginReact(),
     pluginSvgr({
       svgrOptions: {
